c2e583b73cd5485b16cfbc4d678d1be443ab8682
Keenan Rood <Kmrood1@gmail.com>
2017-04-06 16:51:12 -0500

    finished table functionality

--- index.php
+++ index.php
@@ -34,12 +34,65 @@ and open the template in the editor.
             $pTypes[$host] = $pType;
             $buildings[$host] = $building;
             $roomNums[$host] = $roomNum;
+            
+            if(!isset($printersInBuildings[$building])) {
+              $printersInBuildings[$building] = 0;
+            }
+            
+            $printersInBuildings[$building]++;
           }
-          
-          
-          
+             
         }
         
+        foreach($printersInBuildings as $building=>$numPrinters) {
+            
+            // Mark up each table
+            echo <<<TABLE
+            
+            <table>
+              <tr class="firstRow">
+                <td colspan="3">
+                  <span class="headerText">$building</span>
+                </td>
+                <td>
+                  # of printers = $numPrinters
+                </td>
+              </tr>
+                    
+            
+TABLE;
+            
+            // write the table rows
+            foreach($ips as $host=>$ip) {
+              
+              if($building == $buildings[$host]) {
+                
+                if($pTypes[$host] == "Lexmark") {
+                  $className = "lexmark";
+                } else if($pTypes[$host] == "HP LaserJet") {
+                  $className = "laserJet";
+                } else if($pTypes[$host] == "Epson") {
+                  $className = "epson";
+                } else {
+                  $className = "other";
+                }
+                
+              echo <<<TABLEROW
+
+              <tr>
+                <td class="$className">$host</td>
+                <td>$ip</td>
+                <td>$pTypes[$host]</td>
+                <td>$roomNums[$host]</td>
+              </tr>
+TABLEROW;
+              }
+            }
+            
+            echo " </table>";
+          } // end of for each building
+        
         ?>
+     
     </body>
 </html>
